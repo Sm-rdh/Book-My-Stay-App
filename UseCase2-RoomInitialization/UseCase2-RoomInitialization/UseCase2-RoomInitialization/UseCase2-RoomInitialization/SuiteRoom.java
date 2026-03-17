f7adf041947340af7ebcbb6dd3c9fc3cda0e3eb8
Sm-rdh <samridh.royalrumbles@gmail.com>
2026-03-17 15:36:30 +0530

    Added SuiteRoom class

---
+++ UseCase2-RoomInitialization/UseCase2-RoomInitialization/UseCase2-RoomInitialization/UseCase2-RoomInitialization/SuiteRoom.java
@@ -0,0 +1,11 @@
+public class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Suite";
+    }
+}
