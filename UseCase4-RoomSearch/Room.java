a8cf528325cf4731fb5acbdea96e0254fe29deef
Sm-rdh <samridh.royalrumbles@gmail.com>
2026-03-17 15:46:44 +0530

    Added Room base class for UC4

---
+++ UseCase4-RoomSearch/Room.java
@@ -0,0 +1,22 @@
+public abstract class Room {
+
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public abstract String getRoomType();
+
+    public void displayDetails(int available) {
+        System.out.println(getRoomType() + " Room:");
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+        System.out.println("Available: " + available + "\n");
+    }
+}
