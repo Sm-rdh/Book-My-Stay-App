c0e2f8171d29cafe4c31de7c10f609c546103de9
Sm-rdh <samridh.royalrumbles@gmail.com>
2026-03-17 15:28:06 +0530

    Added serializable inventory service

---
+++ UseCase12-Persistence/InventoryService.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InventoryService implements Serializable {
+
+    private Map<String, Integer> inventory;
+
+    public InventoryService() {
+        inventory = new HashMap<>();
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
+    }
+
+    public Map<String, Integer> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Map<String, Integer> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void displayInventory() {
+        System.out.println("\nCurrent Inventory:");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
