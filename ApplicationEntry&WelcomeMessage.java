75fd2279e820ccfeecd8ccac26129ef5a43e8bd6
Sm-rdh <samridh.royalrumbles@gmail.com>
2026-03-17 14:31:30 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ ApplicationEntry&WelcomeMessage.java
@@ -0,0 +1,20 @@
+/**
+ * UseCase1HotelBookingApp
+ * 
+ * Entry point of the Book My Stay application.
+ * Displays welcome message and version info.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("        Version: 1.0                ");
+        System.out.println("====================================");
+
+    }
+}
